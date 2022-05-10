7aa480ffb3f0752323eb1dc305f9995388e2291b
Anahid <anahidhemmat2130@gmail.com>
2022-05-10 15:14:40 +0400

    login and logout form is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
+
 function App() {
-  return <div className="App"></div>;
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+  });
+  const [error, setError] = useState("");
+
+  const login = (details) => {
+    console.log(details);
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not match!");
+      setError("Details do not match!");
+    }
+  };
+
+  const logout = () => {
+    console.log("Logout");
+    setUser({
+      name: "",
+      email: "",
+    });
+  };
+  return (
+    <div className="App">
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>Welcome, {user.name}</h2>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm login={login} error={error} />
+      )}
+    </div>
+  );
 }
 
 export default App;
